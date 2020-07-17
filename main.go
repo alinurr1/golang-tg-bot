f567e268c1cddfaffbc82b5ff44a6b5d8befbf49
Alinur <alinur.amangazy@nu.edu.kz>
2020-07-18 00:20:37 +0600

    Handlers added

--- main.go
+++ main.go
@@ -29,9 +29,32 @@ func main() {
 		log.Fatal(err)
 	}
 
+	b.Handle("/poem", func(m *tb.Message) {
+		b.Send(m.Sender, 
+			`The lazy artist-boor is blacking
+			The genius's picture with his stuff,
+			Without any sense a-making
+			His low drawing above.
+			
+			But alien paints, in stride of years,
+			Are falling down as a dust,
+			The genius's masterpiece appears
+			With former brilliance to us.
+			
+			Like this, the darkly apparitions
+			Are leaving off my tortured heart,
+			And it again revives the visions
+			Of virgin days I left behind.`)
+		b.Send(m.Sender, `By Aleksandr Pushkin`)
+	})
+
 	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Greetings, soul")
+	})
+
+	b.Handle("", func(m *tb.Message) {
 		b.Send(m.Sender, "You entered "+m.Text)
-	  })
+	})
 	  
 	b.Start()
 }
